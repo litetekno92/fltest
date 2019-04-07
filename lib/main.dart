23a2a1040c6b51bee149057256b2d00eb9cc6f24
LiteTekno92 <lite.tekno92@gmail.com>
2019-04-07 11:25:26 +0200

    new drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:menu/menu.dart';
 
-void main() => runApp(MyApp());
+const String _AccountName = 'Aravind Vemula';
+const String _AccountEmail = 'vemula.aravind336@gmail.com';
+const String _AccountAbbr = 'AV';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() => runApp(new Keeper());
+
+class Keeper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return new MaterialApp(
+      title: 'Keeper',
+      home: new KeeperDrawer(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
+class KeeperDrawer extends StatelessWidget {
+  Drawer drawer = new Drawer(
+    child: new Column(
+      children: <Widget>[
+        new UserAccountsDrawerHeader(
+          accountName: const Text(_AccountName),
+          accountEmail: const Text(_AccountEmail),
+          currentAccountPicture: new CircleAvatar(
+            backgroundColor: Colors.brown,
+            child: new Text(_AccountAbbr)
+          ),
+        )
+      ]
+    )
+  );
+	
+	@override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return new Scaffold(
+      drawer: drawer,
+      appBar: new AppBar(
+        title: new Text('Keeper'),
       ),
-      body: Column(
-        children: <Widget>[
-          Text(
-            _counter.toString(),
-          ),
-          Menu(
-            child: Container(
-              width: 200,
-              color: Colors.yellow,
-              height: 100,
-              child: Text("long press show menu"),
-            ),
-            items: [
-              MenuItem("copy", () {}),
-              MenuItem("add", _incrementCounter),
-              MenuItem("copy", () {}),
-            ],
-            decoration: MenuDecoration(
-              // constraints: BoxConstraints(),
-              padding: const EdgeInsets.all(10),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: new Center(
+        child: new Text('Hello World!')
+      )
     );
   }
 }
-
