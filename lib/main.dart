b1ddfb7068f64dcb072478690f400731ab57b8d2
LiteTekno92 <lite.tekno92@gmail.com>
2019-04-11 11:43:18 +0200

    Testing tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,115 @@
 import 'package:flutter/material.dart';
 
-const String _AccountName = 'Aravind Vemula';
-const String _AccountEmail = 'vemula.aravind336@gmail.com';
-const String _AccountAbbr = 'AV';
+class AppBarBottomSample extends StatefulWidget {
+  @override
+  _AppBarBottomSampleState createState() => _AppBarBottomSampleState();
+}
 
-void main() => runApp(new Keeper());
+class _AppBarBottomSampleState extends State<AppBarBottomSample> with SingleTickerProviderStateMixin {
+  TabController _tabController;
 
-class Keeper extends StatelessWidget {
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: choices.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void _nextPage(int delta) {
+    final int newIndex = _tabController.index + delta;
+    if (newIndex < 0 || newIndex >= _tabController.length)
+      return;
+    _tabController.animateTo(newIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Keeper',
-      home: new KeeperDrawer(),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('AppBar Bottom Widget'),
+          leading: IconButton(
+            tooltip: 'Previous choice',
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () { _nextPage(-1); },
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.arrow_forward),
+              tooltip: 'Next choice',
+              onPressed: () { _nextPage(1); },
+            ),
+          ],
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(48.0),
+            child: Theme(
+              data: Theme.of(context).copyWith(accentColor: Colors.white),
+              child: Container(
+                height: 48.0,
+                alignment: Alignment.center,
+                child: TabPageSelector(controller: _tabController),
+              ),
+            ),
+          ),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: choices.map<Widget>((Choice choice) {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ChoiceCard(choice: choice),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
 
-class KeeperDrawer extends StatelessWidget {
-  Drawer drawer = new Drawer(
-    child: new Column(
-      children: <Widget>[
-        new UserAccountsDrawerHeader(
-          accountName: const Text(_AccountName),
-          accountEmail: const Text(_AccountEmail),
-          currentAccountPicture: new CircleAvatar(
-            backgroundColor: Colors.brown,
-            child: new Text(_AccountAbbr)
-          ),
-        )
-      ]
-    )
-  );
-	
-	@override
+class Choice {
+  const Choice({ this.title, this.icon });
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = <Choice>[
+  Choice(title: 'CAR', icon: Icons.directions_car),
+  Choice(title: 'BICYCLE', icon: Icons.directions_bike),
+  Choice(title: 'BOAT', icon: Icons.directions_boat),
+  Choice(title: 'BUS', icon: Icons.directions_bus),
+  Choice(title: 'TRAIN', icon: Icons.directions_railway),
+  Choice(title: 'WALK', icon: Icons.directions_walk),
+];
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({ Key key, this.choice }) : super(key: key);
+
+  final Choice choice;
+
+  @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      drawer: drawer,
-      appBar: new AppBar(
-        title: new Text('Keeper'),
+    final TextStyle textStyle = Theme.of(context).textTheme.display1;
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(choice.icon, size: 128.0, color: textStyle.color),
+            Text(choice.title, style: textStyle),
+          ],
+        ),
       ),
-      body: new Center(
-        child: new Text('Hello World!')
-      )
     );
   }
+}
+
+void main() {
+  runApp(AppBarBottomSample());
 }
